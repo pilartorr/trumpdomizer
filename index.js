ed3bac423120d1dd1583e33bdfd42178d896cf0a
pilartorr <sauleturme@gmail.com>
2019-12-11 14:21:38 +0100

    sending index.html to heroku

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({hola:'zarvi trumpdomizer'});
-})
+const path = require('path');
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);

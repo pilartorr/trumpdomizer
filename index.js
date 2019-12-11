e50b48edb08268bcbca4aa9952cd901a047bed58
pilartorr <sauleturme@gmail.com>
2019-12-11 14:47:32 +0100

    testing sendFile function

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('client/build'));
-
 const path = require('path');
-app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+app.use(express.static('client/public'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;

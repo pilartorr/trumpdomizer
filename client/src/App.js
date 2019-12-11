fa404b5b7e20d0c8734dba893a32b75c7b096307
pilartorr <sauleturme@gmail.com>
2019-12-11 14:09:25 +0100

    responsive design

--- client/src/App.js
+++ client/src/App.js
@@ -6,29 +6,38 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      randomNumber: 0
+      amount: 6,
+      randoms: []
     };
-    this.handleClick = this.handleClick.bind(this);
+
   }
 
-  handleClick() {
-     const min = 0;
-     const max = 9;
-     const randFloorNumber = Math.floor(min + Math.random() * (max - min));
+  componentDidMount() {
+      this.handleClick();
+  }
 
-     this.setState({
-       randomNumber: randFloorNumber
-     });
-   }
+  handleClick = () => {
+    const min = 0;
+    const max = 9;
+    const randFloorNumbers = [];
+
+    for (let i = 0; i < this.state.amount; i++) {
+        const randFloorNumber = Math.floor(min + Math.random() * (max - min));
+        randFloorNumbers.push(randFloorNumber);
+    }
+      this.setState({
+        randoms: randFloorNumbers
+      })
+  }
 
   render() {
-
-    const divs = [1, 2, 3, 4, 5, 6, 7];
-    let div = divs.map( div =>
-        <div key={div} className="App-number-div col border border-info">
-           {this.state.randomNumber}
+    const randoms = this.state.randoms.map((random, i) => {
+        return (
+        <div key={i} className="App-number-div col-3 col-lg border border-info lead">
+            {random}
         </div>
-    );
+        )
+    })
 
     return (
       <div className="App">
@@ -36,17 +45,17 @@ class App extends Component {
         <h2 className="App-subtitle h3 lead">Make Random Great Again</h2>
         <img className="App-logo rounded" src={logo} alt="Trumpdomizer Logo" onClick={this.handleClick} />
         <p className="App-click-text mt-4 h4 font-weight-bold">CLICK TRUMP!</p>
-        <div className="container mt-4 mx-auto">
+        <div className="container">
           <div className="App-numbers-container d-flex row mx-auto">
-            {div}
+            {randoms}
           </div>
         </div>
-        <p className="App-description mx-auto mt-4">lottery random numbers extracted from tweets containing the endpoint "trump".
+        <p className="App-description mx-auto">lottery random numbers extracted from tweets containing the endpoint "trump".
            This study is inspire, among other things, in the project random.org from
            Dr Mads Haahr of the School of Computer Science and Statistics,
            where it produce random numbers based on atmosferic noise...
         </p>
-        <p className="text-muted">&copy; 2019 trumpdomizer.com</p>
+        <p className="App-copyRight text-muted">&copy; 2019 trumpdomizer.com</p>
       </div>
     );
   }
